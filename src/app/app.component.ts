ed0607bb509605faa57ff0a8cc818618664a81c0
IkbalKulahcioglu <ikbalkulahcioglu@gmail.com>
2022-12-07 23:21:17 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage =0;
+  images = [
+    {
+      title:'Beach',
+      url:'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title:'Forest',
+      url:'https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Rm9yZXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title:'Town',
+      url:'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Q0lUWXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title:'Mountain',
+      url:'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+    }
+  ]
+
+
 }
